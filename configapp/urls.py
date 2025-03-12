ac776a751798372448ba0619e77d763297fb7e6d
AkbarxonFayzullayev <akbarxonfayzullayev555@gmail.com>
2025-03-12 17:53:20 +0500

    tayyor

--- configapp/urls.py
+++ configapp/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('index/',index, name='index'),
+    path('index/',index, name='home'),
     path('categories/<int:category_id>',categories,name='categories'),
     path('new_about/<int:new_id>',new_about,name='new_about'),
     path('add_news/',add_news,name='add_news'),
